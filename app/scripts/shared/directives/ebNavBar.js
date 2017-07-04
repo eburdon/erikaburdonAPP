82a296905791c4f6ba78e3ed8270d9490ec2adf0
Erika Burdon <erikaeburdon@gmail.com>
2017-07-03 20:51:51 -0700

    New homepage and navbar; mass cleanup

---
+++ app/scripts/shared/directives/ebNavBar.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular
+  .module('erikaburdonApp')
+  .directive('ebNavBar', function() {
+    return {
+      templateUrl: 'scripts/shared/directives/ebNavBar.html'
+    };
+  });
